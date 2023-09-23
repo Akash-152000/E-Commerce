e44784ac963ee2963f7460f3c3f0948662a4b4bc
Akash-152000 <akash152000akash@gmail.com>
2023-09-23 20:50:25 +0530

    Created routes for products

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,23 +1,65 @@
-const Product = require('../models/productModel')
-
+const Product = require("../models/productModel");
 
 // Create a Product - ADMIN
 
-exports.createProduct = async (req,res,next) =>{
-    const product = await Product.create(req.body)
+exports.createProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
 
-    res.status(201).json({
-        success:true,
-        product
-    })
-}
-
+  res.status(201).json({
+    success: true,
+    product,
+  });
+};
 
 // Get all Products
-exports.getAllProducts = async(req,res) =>{
-    const products = await Product.find({})
-    res.status(200).json({
-        success:true,
-        products
-    })
-}
+exports.getAllProducts = async (req, res, next) => {
+  const products = await Product.find();
+  res.status(200).json({
+    success: true,
+    products,
+  });
+};
+
+// Update product --Admin
+
+exports.updateProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+// Delete Product -- Admin
+
+exports.deleteProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+
+//   console.log(product);
+  await product.deleteOne({_id:req.params.id});          // remove() is deprecated
+
+  res.status(200).json({
+    success: true,
+    message: `${product.name} has been deleted`,
+  });
+};
